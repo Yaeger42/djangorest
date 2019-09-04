22320c103efd4d63e1007859119a6a15b5cf8f24
Diego Casillas <diego.casillas@flex.com>
2019-09-04 09:25:16 -0500

    Tests done in first stage

--- djangorest/api/models.py
+++ djangorest/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class BucketList(models.Model):
+    #This class represents the bucketlist model.
+    name = models.CharField(max_length = 255, blank = False, unique = True)
+    date_created = models.DateTimeField(auto_now_add = True)
+    date_modified = models.DateTimeField(auto_now = True)
+    #We use models.DateTimeField and auto_now_add to add the dates automatically
+
+    def __str__(self):
+        #Return a human readable representation of the model instance
+        return "{}".format(self.name)
--- djangorest/api/tests.py
+++ djangorest/api/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
-
+from .models import BucketList
 # Create your tests here.
+class ModelTestCase(TestCase):
+    #This class defines the test suite for the bucketlist model"
+    
+    def setUp(self):
+        self.bucketlist_name = "Write world class code"
+        self.bucketlist = BucketList(name = self.bucketlist_name)
+
+    def test_model_can_create_a_bucketlist(self):
+        #Test the bucketlist model can create a bucketlist
+        old_count = BucketList.objects.count()
+        self.bucketlist.save()
+        new_count = BucketList.objects.count()
+        self.assertNotEqual(old_count, new_count)
+
+    
